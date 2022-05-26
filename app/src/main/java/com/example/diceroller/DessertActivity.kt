1cc00fa374e3480fc744d85e8387a93c4e345831
JS Kim <splineage@naver.com>
2022-05-26 23:20:13 +0900

    Dessert

---
+++ app/src/main/java/com/example/diceroller/DessertActivity.kt
@@ -0,0 +1,118 @@
+package com.example.diceroller
+
+import android.content.ActivityNotFoundException
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ShareCompat
+import com.example.diceroller.databinding.ActivityDessertBinding
+
+const val KEY_REVENUE = "revenue_key"
+const val KEY_DESSERT_SOLE = "dessert_sold_key"
+class DessertActivity: AppCompatActivity() {
+    private var revenue = 0
+    private var dessertsSold = 0
+
+    private lateinit var binding: ActivityDessertBinding
+
+    data class Dessert(
+        val imageId: Int,
+        val price: Int,
+        val startProductionAmount: Int
+    )
+
+    // Create a list of all desserts, in order of when they start being produced
+    private val allDesserts = listOf(
+        Dessert(R.drawable.cupcake, 5, 0),
+        Dessert(R.drawable.donut, 10, 5),
+        Dessert(R.drawable.eclair, 15, 20),
+        Dessert(R.drawable.froyo, 30, 50),
+        Dessert(R.drawable.gingerbread, 50, 100),
+        Dessert(R.drawable.honeycomb, 100, 200),
+        Dessert(R.drawable.icecreamsandwich, 500, 500),
+        Dessert(R.drawable.jellybean, 1000, 1000),
+        Dessert(R.drawable.kitkat, 2000, 2000),
+        Dessert(R.drawable.lollipop, 3000, 4000),
+        Dessert(R.drawable.marshmallow, 4000, 8000),
+        Dessert(R.drawable.nougat, 5000, 16000),
+        Dessert(R.drawable.oreo, 6000, 20000)
+    )
+
+    private var currentDessert = allDesserts[0]
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDessertBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if (savedInstanceState != null){
+            revenue = savedInstanceState.getInt(KEY_REVENUE, 0)
+            dessertsSold = savedInstanceState.getInt(KEY_DESSERT_SOLE, 0)
+            showCurrentDessert()
+        }
+        binding.dessertButton.setOnClickListener {
+            onDessertClicked()
+        }
+
+        binding.revenue = revenue
+        binding.amountSold = dessertsSold
+
+        binding.dessertButton.setImageResource(currentDessert.imageId)
+
+    }
+
+    private fun onDessertClicked(){
+        revenue += currentDessert.price
+        dessertsSold++
+
+        binding.revenue = revenue
+        binding.amountSold = dessertsSold
+
+        showCurrentDessert()
+    }
+
+    private fun showCurrentDessert(){
+        var newDessert = allDesserts[0]
+        for (dessert in allDesserts){
+            if (dessertsSold >= dessert.startProductionAmount){
+                newDessert = dessert
+            }
+            else break
+        }
+        if (newDessert != currentDessert){
+            currentDessert = newDessert
+            binding.dessertButton.setImageResource(newDessert.imageId)
+        }
+    }
+
+    private fun onShare(){
+        val shareIntent = ShareCompat.IntentBuilder.from(this)
+            .setText(getString(R.string.share_text, dessertsSold, revenue))
+            .setType("text/plain")
+            .intent
+        try {
+            startActivity(shareIntent)
+        }catch (e: ActivityNotFoundException){
+            Toast.makeText(this, getString(R.string.sharing_not_available), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.dessert_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.shareMenuButton-> onShare()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_REVENUE, revenue)
+        outState.putInt(KEY_DESSERT_SOLE, dessertsSold)
+    }
+}
