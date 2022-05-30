21ca4a8fb3473bec83627c706cab0f89d4ddf2ee
JS Kim <splineage@naver.com>
2022-05-30 11:26:42 +0900

    Test LiveData & ViewModel

--- cupcake/src/test/java/com/example/cupcake/ExampleUnitTest.kt
+++ cupcake/src/test/java/com/example/cupcake/ExampleUnitTest.kt
@@ -1,8 +1,12 @@
 package com.example.cupcake
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.cupcake.model.OrderViewModel
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Rule
+import java.text.NumberFormat
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +14,21 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun quantity_twelve_cupcakes(){
+        val viewModel = OrderViewModel()
+        viewModel.quantity.observeForever{}
+        viewModel.setQuantity(12)
+        assertEquals(12, viewModel.quantity.value)
+    }
+    @Test
+    fun price_twelve_cupcakes(){
+        val viewModel = OrderViewModel()
+        viewModel.price.observeForever{}
+        viewModel.setDate(viewModel.dateOptions[0])
+        viewModel.setQuantity(12)
+        assertEquals(NumberFormat.getCurrencyInstance().format(27), viewModel.price.value)
     }
 }
