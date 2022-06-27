f7f0383115178ee44cbeea5e53f9e7c863639a73
JS Kim <splineage@naver.com>
2022-06-27 18:18:04 +0900

    delete update

--- inentory_app/src/main/java/com/onethefull/inentory_app/AddItemFragment.kt
+++ inentory_app/src/main/java/com/onethefull/inentory_app/AddItemFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -27,6 +28,8 @@ class AddItemFragment : Fragment() {
     private val navigationArgs: ItemDetailFragmentArgs by navArgs()
     private var _binding: FragmentAddItemBinding? = null
     private val binding get() = _binding!!
+    lateinit var item: Item
+
     // by activityViewModels Kotlin 속성 위임을 사용하여 프래그먼트 전체에서 ViewModel 을 공유.
     private val viewModel: InventoryViewModel by activityViewModels{
         InventoryViewModelFactory(
@@ -51,9 +54,18 @@ class AddItemFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.saveAction.setOnClickListener {
-            addNewItem()
+        val id = navigationArgs.itemId
+        if (id > 0){
+            viewModel.retrieveItem(id).observe(this.viewLifecycleOwner){selectedItem ->
+                item = selectedItem
+                bind(item)
+            }
+        }else{
+            binding.saveAction.setOnClickListener {
+                addNewItem()
+            }
         }
+
     }
 
     private fun isEntryValid(): Boolean{
@@ -82,6 +94,29 @@ class AddItemFragment : Fragment() {
         val inputMethodManager = requireActivity().getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(requireActivity().currentFocus?.windowToken, 0)
         _binding = null
+    }
+
+    private fun bind(item: Item){
+        val price = "%.2f".format(item.itemPrice)
+        binding.apply {
+            itemName.setText(item.itemName, TextView.BufferType.SPANNABLE)
+            itemPrice.setText(price, TextView.BufferType.SPANNABLE)
+            itemCount.setText(item.quantityInStock.toString(), TextView.BufferType.SPANNABLE)
+            saveAction.setOnClickListener { updateItem() }
+        }
+    }
+
+    private fun updateItem(){
+        if (isEntryValid()){
+            viewModel.updateItem(
+                this.navigationArgs.itemId,
+                this.binding.itemName.text.toString(),
+                this.binding.itemPrice.text.toString(),
+                this.binding.itemCount.text.toString()
+            )
+            val action = AddItemFragmentDirections.actionAddItemFragmentToItemListFragment()
+            findNavController().navigate(action)
+        }
     }
     companion object {
 
